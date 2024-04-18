5be0ea89d79d2039d58cd57146b415e404c2fa88
Svadrut Kukunooru <kukunooru@wisc.edu>
2024-04-17 20:42:17 -0500

    mb forgot to update

---
+++ sem6/compsci537/p3/p3/p3/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  p3
+//
+//  Created by Svadrut on 2/12/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
